5d11a96aaa6f370278b3e3b956c7d93afa4cffef
Jannis Milz <jannis.milz@econ.uzh.ch>
2023-05-10 14:15:02 +0000

    Add tailwindcss config with root vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
 		extend: {
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic":
-					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			colors: {
+				primary: "var(--primary)",
+				secondary: "var(--secondary)",
+				bgPrimary: "var(--bgPrimary)",
+				bgSecondary: "var(--bgSecondary)",
+				textPrimary: "var(--textPrimary)",
+				textSecondary: "var(--textSecondary)",
 			},
+			// Added +200px on every viewport because of sidebar (so it's still responsive)
+			screens: {
+				'sm': '840px',
+				'md': '968px',
+				'lg': '1224px',
+				'xl': '1480px',
+				'2xl': '1736px',
+			}
 		},
 	},
 	plugins: [],
