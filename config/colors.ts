a8950b90c0d5a2eb28b7070d2a55dea2f9b5f23f
Jannis Milz <jannis.milz@econ.uzh.ch>
2023-05-10 14:10:32 +0000

    Created colors config file

---
+++ config/colors.ts
@@ -0,0 +1,10 @@
+const colors = [
+	{ name: "primary", default: "42a6c2" },
+	{ name: "secondary", default: "146C94" },
+	{ name: "bgPrimary", default: "F6F1F1" },
+	{ name: "bgSecondary", default: "e1dede" },
+	{ name: "textPrimary", default: "000000" },
+	{ name: "textSecondary", default: "7a7a7a" },
+];
+
+export default colors;
